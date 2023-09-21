a3d3a1ee7b0fb314b402cffba03495fba2ccfba2
HakimJeck <120010470+HakimJeck@users.noreply.github.com>
2023-09-21 21:33:27 +0700

    js

---
+++ Fsoft/JS/main.js
@@ -0,0 +1,14 @@
+//Number tyoe
+var length = 100;
+var weight = 120;
+//String type
+var color = "red";
+var brand = "Honda"
+//Boolean
+var notTrue = false;
+var isTrue = true;
+//Object type
+var person = {name:"Minh",age : 20};
+var acedemyAddress = "17 Duy Tan";
+console.log(acedemyAddress);
+console.log("Hello JAS");
