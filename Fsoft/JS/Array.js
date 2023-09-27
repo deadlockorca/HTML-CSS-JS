5a2e66eb792e95b3b5f630943a1bfbb7652e95b8
HakimJeck <120010470+HakimJeck@users.noreply.github.com>
2023-09-27 10:10:12 +0700

    array

---
+++ Fsoft/JS/Array.js
@@ -0,0 +1,24 @@
+//let myString = "Manchester, London,Liverpool,Birmingham,Leeds,Carlisle";
+//let myArray = myString.split(',');
+//let  firstElement = myArray[0];
+//let thirElement = myArray[2];
+//let lastElement = myArray[myArray.length - 1];
+//console.log(firstElement);
+//console.log(thirElement);
+//console.log(lastElement);
+
+//myArray.pop();
+
+//let newArrayString = myArray.join(' ');
+//console.log(newArrayString);
+
+let myArray = ['apple','banana','orange'];
+myArray.push('Cardiff');
+console.log(myArray);
+myArray.push('Bradford','Brighton');
+console.log(myArray);
+let arrayLength = myArray.length;
+console.log(arrayLength);
+let lastItem = myArray.pop();
+console.log(lastItem);
+console.log(myArray);
