6ce0aa8b4a15ecbb5af4f1687d86662de0cca2c6
HakimJeck <120010470+HakimJeck@users.noreply.github.com>
2023-10-03 20:05:14 +0700

    .

---
+++ Fsoft/JS/conditions.js
@@ -0,0 +1,49 @@
+var hour = new Date().getHours();
+console.log("now", hour);
+if(hour >= 18){
+    document.getElementById("demo").innerHTML = "Afternoon";
+}else{
+    document.getElementById("demo").innerHTML = "Morning";
+};
+//EX2
+var checkBox = document.querySelector("#shoppingCheck");
+var shoppingText = document.querySelector("#shoppingText");
+var shoppingDone = false; 
+
+checkBox.addEventListener("change",function(){
+    shoppingDone = !shoppingDone;
+    updateAllowance();
+});
+function updateAllowance(){
+    if (shoppingDone === true){
+        var childsAllowance = 10;
+    }else{
+        childsAllowance = 5;
+    }
+    shoppingText.textContent = ` Child has earned $${childsAllowance} this week.`;
+}
+updateAllowance();
+//EX3
+var seclectWeather = document.querySelector("#weather");
+var weatherText = document.querySelector("#weatherText");
+seclectWeather.addEventListener("change", setWeather);
+
+function setWeather(){
+    let choice = seclectWeather.value;
+    let temperature = 30;
+    console.log("choice", choice);
+    if(choice === "sunny" && temperature < 40){
+        weatherText.textContent = "It's nice and sunny outside today"    
+    }else if (choice === "sunny" && temperature >= 40){
+        weatherText.textContent = "It's super hot, dont go out";
+
+    }else if( choice === "rainy"){
+            weatherText.textContent = "Rain is falling outside"
+    }else if( choice === "snowing"){
+        weatherText.textContent = "Snow is comming down"
+    }else if( choice === "overcast"){
+        weatherText.textContent = "It's not raining , but sky is grey"
+    }else{
+        weatherText.textContent = "";
+    }
+}    
